e34073ec6883fc170fcf72b5abf981e25162198e
Genki Nishiyama <genki.nishiyama@gmail.com>
2020-05-12 08:39:57 +0900

    ユーザー新規登録・編集画面修正、パスワード無しで更新可能に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
-
+  # TwitterAPIでアカウント情報を取得
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
 
@@ -20,12 +20,27 @@ class User < ApplicationRecord
         location: auth.info.location
       )
     end
-    
     user
   end
 
+  # プロフィール情報をパスワード無しで更新できるようにする
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+ 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+ 
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
+
   private
 
+  # Eメールが無いと登録できないためダミーアドレス生成
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
